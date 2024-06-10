a1ab3558847952557301a5c020d0dbde4c8ea5aa
Kanza Jovita <kanzajovita@gmail.com>
2024-06-10 16:44:20 +0300

    calculator

---
+++ frecodecamp.py
@@ -0,0 +1,31 @@
+#The for loop to iterate over text.
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text:
+    index = alphabet.find(char)
+    print(char,index)
+
+
+#The for loop to iterate over text.lower().
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text.lower():
+    index = alphabet.find(char)
+    print(char, index)
+
+
+
+#turn the last print() call into print('char:', char, 'new char:', new_char).
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text.lower():
+    index = alphabet.find(char)
+    new_index = index + shift
+    new_char = alphabet[new_index]
+    print('char:',char,'new_char:',new_char)
